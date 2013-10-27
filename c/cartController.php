19d7ff3d538ab40766dfc1ab28a3d32fdb72c38a
reximir <davidlibraboy@gmail.com>
2013-10-28 01:30:35 +0700

    update transaction added

--- c/cartController.php
+++ c/cartController.php
@@ -5,13 +5,11 @@ class CartController extends dasarController{
 		/**dummy mode*/
 		//misal barang yg udh dipilih dibeli kategori kulkas, meja, kursi
 		$cart_cat = array('Kulkas', 'Meja', 'Kursi');
-
 		$_SESSION['cart_cat']=$cart_cat;
 
 		//contoh misal Kulkas dibeli tipe sinyoku, sanyo
 		$cat_kulkas = array('Sanyo' => 3, 'Sinyoku' => 6 ); 
 		$_SESSION['Kulkas']=$cat_kulkas;
-
 
 		$cat_meja = array('chitose' => 1, 'cumi' => 7 ); 
 		$_SESSION['Meja']=$cat_meja;
@@ -19,41 +17,41 @@ class CartController extends dasarController{
 		$cat_kursi = array('ayomi' => 4, 'cokoro' => 4 ); 
 		$_SESSION['Kursi']=$cat_kursi;
 	
-	//test case
+// 	//test case
 
-//update trans
-		//contoh update chitose jadi jumlah 2
-		$_SESSION['Meja']['chitose']=2;
+// //update trans
+// 		//contoh update chitose jadi jumlah 2
+// 		$_SESSION['Meja']['chitose']=2;
 
-//insert new trans 2 case
-		//contoh insert buff ke meja jumlah 1
-		$_SESSION['Meja']['buff']=1;
+// //insert new trans 2 case
+// 		//contoh insert buff ke meja jumlah 1
+// 		$_SESSION['Meja']['buff']=1;
 
-//insert sesuatu baru + kategori baru
-		//contoh kategori baju
-		$ada = 0;
-		foreach ($_SESSION['cart_cat'] as $key) {
-			if ($key == 'baju'){
-				$ada = 1;
-			}
-		}
+// //insert sesuatu baru + kategori baru
+// 		//contoh kategori baju
+// 		$ada = 0;
+// 		foreach ($_SESSION['cart_cat'] as $key) {
+// 			if ($key == 'baju'){
+// 				$ada = 1;
+// 			}
+// 		}
 
-		if ($ada == 0) array_push($_SESSION['cart_cat'], 'Baju');
+// 		if ($ada == 0) array_push($_SESSION['cart_cat'], 'Baju');
 
-		// $cat_baju = array('Gio' => 3);
-		$_SESSION['Baju']['Gio'] = 3;
+// 		// $cat_baju = array('Gio' => 3);
+// 		$_SESSION['Baju']['Gio'] = 3;
 
-//delete trans 2 case 
-		//contoh delete sanyo from cart
-		array_splice($_SESSION['Kulkas'], 0,1);
+// //delete trans 2 case 
+// 		//contoh delete sanyo from cart
+// 		array_splice($_SESSION['Kulkas'], 0,1);
 
-		//terus delete Sinyoku
-		array_splice($_SESSION['Kulkas'], 0,1);
+// 		//terus delete Sinyoku
+// 		array_splice($_SESSION['Kulkas'], 0,1);
 
-//penanganan delete semua anak kategori itu
-		if ($_SESSION['Kulkas'] == null){
-			array_splice($_SESSION['cart_cat'], 0,1);
-		}
+// //penanganan delete semua anak kategori itu
+// 		if ($_SESSION['Kulkas'] == null){
+// 			array_splice($_SESSION['cart_cat'], 0,1);
+// 		}
 
 
 
@@ -80,53 +78,51 @@ class CartController extends dasarController{
 	}
 
 	/**not tested yet*/
-	//input harus $Quantity >=0
-	public function update($categ = 'Kulkas', $merk = 'Sanyo', $Quantity = 3){
+	
+	public function update($categ = 'Kulkas', $merk = 'Sanyo', $Quantity = 0){
 		//under maintenance
-		if ($Quantity <=0){//delete the merk
-		
+		if ($Quantity <=0){ //delete
+
+					$b =0; 
+					foreach ($_SESSION['cart_cat'] as $key) {
+						if ($key == $categ){
+							$i = $b; 
+						}
+						$b++;
+					}
+					array_splice($_SESSION[$categ], $i,1);//ex: delete the sanyo from kulkas 
+
+					$idx =0;
+					foreach ($_SESSION['cart_cat'] as $key) {
+						if ($key == $categ){
+							$id = $idx; 
+						}
+						$idx++;
+					}
+					array_splice($_SESSION['cart_cat'], $id,1); //ex : delete category kulkas 
 		}
-		else{
-		
+		else{ //update or insert
+				$ada = 0;
+				foreach ($_SESSION['cart_cat'] as $key) {
+					if ($key == $categ){
+						$ada = 1;
+					}
+				}
+				if ($ada == 0) array_push($_SESSION['cart_cat'], $categ);
+
+				$_SESSION[$categ][$merk]= $Quantity;
+				
+
+
 		}
+		// echo $categ; echo $merk;
+		// echo $_SESSION[$categ][$merk];
 
-
-		//test case
-//update trans
-		//contoh update chitose jadi jumlah 2
-		
-		$_SESSION['Meja']['chitose']=2;
-
-//insert new trans 2 case
-		//contoh insert buff ke meja jumlah 1
-		$_SESSION['Meja']['buff']=1;
-
-//insert sesuatu baru + kategori baru
-		//contoh kategori baju
-		$ada = 0;
-		foreach ($_SESSION['cart_cat'] as $key) {
-			if ($key == 'baju'){
-				$ada = 1;
-			}
-		}
-
-		if ($ada == 0) array_push($_SESSION['cart_cat'], 'Baju');
-
-		// $cat_baju = array('Gio' => 3);
-		$_SESSION['Baju']['Gio'] = 3;
-
-//delete trans 2 case 
-		//contoh delete sanyo from cart
-		array_splice($_SESSION['Kulkas'], 0,1);
-
-		//terus delete Sinyoku
-		array_splice($_SESSION['Kulkas'], 0,1);
-
-//penanganan delete semua anak kategori itu
-		if ($_SESSION['Kulkas'] == null){
-			array_splice($_SESSION['cart_cat'], 0,1);
-		}
-
+		 $model = new Barang();
+		 $template = $this->brankas->template;
+		 $template->view = "cart";
+		 $template->model = $model;
+		 $template->show('layout');
 
 	}
 	
